0d4f89172eca63dfb78a08d4e7eb8b68433205eb
marishkazachariah <marishka.zachariah@gmail.com>
2023-10-25 18:46:27 +0200

    complete all of week 08 day 03 exercises

--- src/main/java/week_8/day_3/Main.java
+++ src/main/java/week_8/day_3/Main.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static final String CSV_FILE_PATH = "src/main/java/week_8/day_3/users.csv";
 
     // Exercise 1
@@ -83,7 +82,7 @@ public class Main {
     // Exercise 4
     public static void searchUser() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter search query (either name, email, or phone number): ");
+        System.out.println("Enter search query (either name, email, or phone number): ");
         String query = scanner.nextLine().toLowerCase();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(CSV_FILE_PATH))) {
             String line;
@@ -137,8 +136,10 @@ public class Main {
             }
             if (query == 1) {
                 users.sort(Comparator.comparing(User::getAge));
+                updateCSVFile(users);
             } else if (query == 2) {
                 users.sort(Comparator.comparing(User::getName));
+                updateCSVFile(users);
             } else {
                 System.out.println("Invalid input.");
             }
@@ -159,6 +160,81 @@ public class Main {
     }
 
     // Exercise 6
+    public static void updateUsersAddress() {
+        List<User> users = readUserData();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name of the user whose address you want to edit: ");
+        String query = scanner.nextLine().toLowerCase();
+
+        User userToUpdate = null;
+        for (User user : users) {
+            if (user.getName().toLowerCase().contains(query)) {
+                userToUpdate = user;
+                break;
+            }
+        }
+        if (userToUpdate != null) {
+            System.out.println("Enter the new address for " + userToUpdate.getName() + ":");
+            String newAddress = scanner.nextLine();
+            userToUpdate.setAddress(newAddress);
+            updateCSVFile(users);
+            System.out.println("Address updated successfully!");
+        } else {
+            System.out.println("User not found.");
+        }
+        displayUserData(users);
+    }
+
+    // Copied to follow editing solution
+    private static List<User> readUserData() {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(CSV_FILE_PATH))) {
+            String line;
+            reader.readLine(); // Skip header line
+            while ((line = reader.readLine()) != null) {
+                String[] userData = line.split(",");
+                String name = userData[0].trim();
+                int age = Integer.parseInt(userData[1].trim());
+                String email = userData[2].trim();
+                String phone = userData[3].trim();
+                String address = userData[4].trim();
+                users.add(new User(name, age, email, phone, address));
+            }
+        } catch (IOException e) {
+            System.out.println("Error occurred while reading the CSV file: " + e.getMessage());
+        }
+        return users;
+    }
+
+    // This is basically Exercise 10
+    private static void updateCSVFile(List<User> users) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(CSV_FILE_PATH))) {
+            writer.write("Name, Age, Email, Phone, Address");
+            writer.newLine();
+            for (User user : users) {
+                writer.write(user.getName() + ", " + user.getAge() + ", " +
+                        user.getEmail() + ", " + user.getPhoneNumber() + ", " + user.getAddress());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error occurred while updating the CSV file: " + e.getMessage());
+        }
+    }
+
+    private static void displayUserData(List<User> users) {
+        System.out.println("User Data from CSV:");
+        for (User user : users) {
+            System.out.println("Name: " + user.getName());
+            System.out.println("Age: " + user.getAge());
+            System.out.println("Email: " + user.getEmail());
+            System.out.println("Phone: " + user.getPhoneNumber());
+            System.out.println("Address: " + user.getAddress());
+            System.out.println("----------------------");
+        }
+    }
+
+    // my former solution that did not override the CSV file
     public static void updateUserAddress() {
         Scanner scanner = new Scanner(System.in);
 
@@ -169,6 +245,7 @@ public class Main {
             String line;
             String header = bufferedReader.readLine();
             String[] headers = header.split(",");
+            List<User> users = new ArrayList<>();
 
             while ((line = bufferedReader.readLine()) != null) {
                 String[] fields = line.split(",");
@@ -177,6 +254,7 @@ public class Main {
                 String email = fields[2].toLowerCase().trim();
                 String phoneNumber = fields[3].toLowerCase().trim();
                 String address = fields[4].toLowerCase().trim();
+
                 if (name.contains(query)) {
                     System.out.println("User to edit: ");
                     System.out.println(headers[0] + ": " + name);
@@ -184,20 +262,71 @@ public class Main {
                     System.out.println(headers[2] + ": " + email);
                     System.out.println(headers[3] + ": " + phoneNumber);
                     System.out.println(headers[4] + ": " + address);
+
                     System.out.println("Enter the new address");
-                    String newAddress = scanner.nextLine();
-                    address = newAddress;
+                    address = scanner.nextLine();
+
                     System.out.println("Edited Address");
                     System.out.println(headers[0] + ": " + name);
                     System.out.println(headers[1] + ": " + age);
                     System.out.println(headers[2] + ": " + email);
                     System.out.println(headers[3] + ": " + phoneNumber);
                     System.out.println(headers[4] + ": " + address);
-                }
 
+                    users.add(new User(name, Integer.parseInt(age), email, phoneNumber, address));
+                }
+                users.add(new User(name, Integer.parseInt(age), email, phoneNumber, address));
             }
         } catch (IOException e) {
-            System.err.println("Error reading and/or searching file: " + e.getMessage());
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+        scanner.close();
+    }
+
+    // Exercise 7
+    public static void deleteUserData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name or email of the user you want to delete: ");
+        String query = scanner.nextLine().toLowerCase();
+        List<User> users = readUserData();
+
+        boolean removed = false;
+
+        for (User user : users) {
+            if (user.getName().equalsIgnoreCase(query) || user.getEmail().equalsIgnoreCase(query)) {
+                users.remove(user);
+                removed = true;
+            }
+        }
+
+        if (removed) {
+            System.out.println("User data removed successfully!");
+            updateCSVFile(users);
+        } else {
+            System.out.println("User not found.");
+        }
+        displayUserData(users);
+        scanner.close();
+    }
+
+    // Exercise 8
+    public static void menu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose from the number of options");
+        System.out.println("1. add new user");
+        System.out.println("2. sort user by age or name");
+        System.out.println("3. edit user's address");
+        System.out.println("4. delete user info");
+        System.out.println("5. search user");
+
+        int input = scanner.nextInt();
+        switch (input) {
+            case 1 -> appendUserCsvFile();
+            case 2 -> sortCsvByAgeOrName();
+            case 3 -> updateUsersAddress();
+            case 4 -> deleteUserData();
+            case 5 -> searchUser();
+            default -> System.out.println("Invalid input/option");
         }
         scanner.close();
     }
@@ -230,10 +359,6 @@ public class Main {
     public static void main(String[] args) {
         writeUsersToCsvFile();
         readUserCsvFile();
-        appendUserCsvFile();
-        searchUser();
-        sortCsvByAgeOrName();
-        updateUserAddress();
-        System.out.println("Number of users: " + totalNumberOfUsers());
+        menu();
     }
 }
