b40046348993f77f25da8bc46d145e3bb1f6bd13
marishkazachariah <marishka.zachariah@gmail.com>
2023-11-02 16:27:26 +0100

    complete w09d04 exercises 1-3

---
+++ src/main/java/week_9/mini_project/day_4/EmployeeReport.java
@@ -0,0 +1,8 @@
+package week_9.mini_project.day_4;
+
+public class EmployeeReport implements Report{
+    @Override
+    public void generateReport() {
+        System.out.println("Generating Employee Report");
+    }
+}
